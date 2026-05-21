3a823e8fb0d2f8c489c3d7a944c5fdbb86b0280a
GreenMan2022 <96194872+GreenMan2022@users.noreply.github.com>
2026-05-21 23:06:06 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,7 @@
-const CACHE_NAME = 'rpg-pwa-v1';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json',
-  'https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js'
-];
-
+const CACHE_NAME = 'rpg-pwa-v2';
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
